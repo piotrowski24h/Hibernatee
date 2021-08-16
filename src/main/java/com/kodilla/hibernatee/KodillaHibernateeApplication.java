bbb85e7fc59e8e18658d7f0c98098b7714c50f8c
Krystian Piotrowski <piotrowski24h@interia.pl>
2021-08-16 19:33:26 +0200

    a

---
+++ src/main/java/com/kodilla/hibernatee/KodillaHibernateeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernatee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernateeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/hibernatee/KodillaHibernateeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.hibernatee;
+
+import org.testng.annotations.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
